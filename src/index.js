eee51acbc536acd520f04479278a1fe48b0717cd
shaikh aaref hamza <skaarefz@gmail.com>
2025-01-09 20:49:16 +0530

    first logic and apierr and response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import express from 'express'
+import { ApiError } from '../utiles/API-ERRORS.js'
+import { ApiResponse } from '../utiles/API-RESPONSE.js'
+ 
 
 const app=express()
+app.use(express.json())
+
+
+
+
+
 const port=process.env.PORT || 3000
 app.get('/',(req,res)=>{
  res.send('hello coder')
+})
+
+
+app.post('/add',(req,res)=>{
+
+ const {name}=req.body
+ if(!name){
+  throw new ApiError(401,'name is complasary')
+ }
+ const data=[];
+ data.push(req.body)
+
+ return res.status(200).json({
+  
+ })
+ 
+
+
+  
 })
 
 app.listen(port,()=>{
